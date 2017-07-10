af1e618ed5b888c6d543c489771a1544b0b2c556
devk <devkordesg@gmail.com>
2017-07-11 00:34:39 +0200

    Qsort benchmark route implemented

--- routes/web.php
+++ routes/web.php
@@ -30,3 +30,47 @@ $app->get('api/all_db_results', function () use ($app) {
 $app->get('api/string_result', function () use ($app) {
     return response("Lorem ipsum");
 });
+
+$app->get('api/qsort_result', function () {
+    $numbers = json_decode(file_get_contents(storage_path('numbers.json')));
+
+    $start = microtime(true);
+    quick_sort($numbers);
+    $elapsed = microtime(true) - $start;
+
+    return "Sorted " . count($numbers) . " time sorting: " . number_format($elapsed * 1000, 4) . "ms";
+});
+
+// http://andrewbaxter.net/quicksort.php
+function quick_sort($array)
+{
+    // find array size
+    $length = count($array);
+
+    // base case test, if array of length 0 then just return array to caller
+    if($length <= 1){
+        return $array;
+    }
+    else{
+
+        // select an item to act as our pivot point, since list is unsorted first position is easiest
+        $pivot = $array[0];
+
+        // declare our two arrays to act as partitions
+        $left = $right = array();
+
+        // loop and compare each item in the array to the pivot value, place item in appropriate partition
+        for($i = 1; $i < count($array); $i++)
+        {
+            if($array[$i] < $pivot){
+                $left[] = $array[$i];
+            }
+            else{
+                $right[] = $array[$i];
+            }
+        }
+
+        // use recursion to now sort the left and right lists
+        return array_merge(quick_sort($left), array($pivot), quick_sort($right));
+    }
+}
